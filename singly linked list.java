b4178ddde7f44a85d4c57b2b8191046d9cee81c4
Jaya-Akshithaa-Gurusamy <23cs034@drngpit.ac.in>
2025-08-07 13:52:37 +0530

    Create singly linked list.java

---
+++ singly linked list.java
@@ -0,0 +1,67 @@
+class node{
+    int data;
+    node next;
+    node(int data){
+        this.data=data;
+        this.next=null;
+    }
+}
+class LinkedList{
+    node head,tail;
+    void insertFirst(int num){
+        node newNode=new node(num);
+        if(head==null){
+            head=newNode;
+            tail=newNode;
+        }else{
+            newNode.next=head;
+            head=newNode;
+        }
+    }
+    void insertMid(int num,int p){
+        node newNode=new node(num);
+        if(head==null){
+            head=newNode;
+            tail=newNode;
+        }else{
+            node fr = head;
+            node bk;
+            for(int i=0;i<p-2;i++)
+                fr=fr.next;
+            bk = fr.next;
+            newNode.next = bk;
+            fr.next = newNode;
+        }
+    }
+    void insertLast(int num){
+        node newNode=new node(num);
+        if(head==null){
+            head=newNode;
+            tail=newNode;
+        }else{
+            tail.next=newNode;
+            tail=newNode;
+        }
+    }
+    void display(){
+        node temp=head;
+        while(temp!=null){
+            System.out.print(temp.data+" ");
+            temp=temp.next;
+        }
+    }
+}
+public class Main
+{
+	public static void main(String[] args) {
+		LinkedList ll=new LinkedList();
+		ll.insertLast(10);
+		ll.insertLast(20);
+		ll.insertFirst(30);
+		ll.insertLast(40);
+		ll.insertLast(50);
+		ll.insertFirst(60);	
+		ll.insertMid(70,3);
+		ll.display();
+	}
+}
